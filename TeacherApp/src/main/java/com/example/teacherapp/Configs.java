93a4088bf6cf360e4f3afac21f1ec7a5b0fdee25
tornadoenergi <91623086+tornadoenergi@users.noreply.github.com>
2023-05-24 14:53:46 +0300

    Delete TeacherApp directory

--- TeacherApp/src/main/java/com/example/teacherapp/Configs.java
+++
@@ -1,9 +0,0 @@
-package com.example.teacherapp;
-
-public class Configs {
-    protected String dbHost = "127.0.0.1";
-    protected String dbPort = "3306";
-    protected String dbUser = "root";
-    protected String dbPass = "root";
-    protected String dbName = "auto_sigin";
-}
--- TeacherApp/src/main/java/com/example/teacherapp/controllers/ControllerTest4.java
+++
@@ -1,4 +0,0 @@
-package com.example.teacherapp.controllers;
-
-public class ControllerTest4 {
-}
